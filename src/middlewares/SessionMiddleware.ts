191e646e38679e6009e4a430e20210b901e109ed
Vagner Cardoso <vagnercardosoweb@gmail.com>
2019-10-05 04:41:12 -0300

    Atualizando dependências

--- src/middlewares/SessionMiddleware.ts
+++ src/middlewares/SessionMiddleware.ts
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import { RequestHandler } from 'express';
+import redis from 'redis';
 import ExpressSession from 'express-session';
 import ConnectRedis from 'connect-redis';
+
 import Middleware from './Middleware';
 
 import configRedis from '../config/redis';
@@ -9,7 +11,8 @@ import configSession from '../config/session';
 
 export default class SessionMiddleware extends Middleware {
   public dispatch(): RequestHandler {
-    const store = new (ConnectRedis(ExpressSession))(configRedis);
+    const client = redis.createClient(configRedis);
+    const store = new (ConnectRedis(ExpressSession))({ client });
 
     return ExpressSession({
       store,
