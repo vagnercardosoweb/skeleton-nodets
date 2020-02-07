c0ccd171e7de4f8742aa1f60588a5cfbaf26e2a2
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-02-07 02:02:22 -0300

    chore: importa a lib Cache e Queue por padrão

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,10 @@
+export { default as Cache } from './Cache';
 export { default as Database } from './Database';
 export { default as HttpRequest } from './HttpRequest';
 export { default as Jwt } from './Jwt';
 export { default as Mailer } from './Mailer';
 export { default as Password } from './Password';
+export { default as Queue } from './Queue';
 export { default as Upload } from './Upload';
 export { default as View } from './View';
 export { default as Yup } from './Yup';
