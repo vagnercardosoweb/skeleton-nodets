339c329d8eeb6bfddafcc052c7a5208bec6e21cb
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-02-16 00:16:33 -0300

    chore: export padrão da lib Queue
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,7 @@ export { default as HttpRequest } from './HttpRequest';
 export { default as Jwt } from './Jwt';
 export { default as Mailer } from './Mailer';
 export { default as Password } from './Password';
+export { default as Queue } from './Queue';
 export { default as Upload } from './Upload';
 export { default as View } from './View';
 export { default as Yup } from './Yup';
