dd8ed6016bd7dcc281151b3e997a52dfe235a019
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-02-16 00:36:22 -0300

    style: CorsMiddleware.ts style guide
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- src/middlewares/CorsMiddleware.ts
+++ src/middlewares/CorsMiddleware.ts
@@ -3,18 +3,20 @@ import { Request, Response, NextFunction, RequestHandler } from 'express';
 
 import configCors from '../config/cors';
 
-export default function CorsMiddleware(): RequestHandler {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const { methods, headers, origin } = configCors;
+export default (): RequestHandler => (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { methods, headers, origin } = configCors;
 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', methods.join(','));
-    res.header('Access-Control-Allow-Headers', headers.join(','));
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Methods', methods.join(','));
+  res.header('Access-Control-Allow-Headers', headers.join(','));
 
-    if (String(req.method).toUpperCase() === 'OPTIONS') {
-      return res.sendStatus(200);
-    }
+  if (String(req.method).toUpperCase() === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
 
-    return next();
-  };
-}
+  return next();
+};
