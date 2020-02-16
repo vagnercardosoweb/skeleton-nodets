d4d1cfe08dda6d95335e6d8be90fd90765f3346a
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-02-16 00:34:31 -0300

    refactor: MorganMiddleware.ts export padrão da própria função
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- src/middlewares/MorganMiddleware.ts
+++ src/middlewares/MorganMiddleware.ts
@@ -1,11 +1,9 @@
 // eslint-disable-next-line no-unused-vars
-import { RequestHandler, Response, Request } from 'express';
+import { Response } from 'express';
 import morgan from 'morgan';
 
-export default function MorganMiddleware(): RequestHandler {
-  const format = process.env.NODE_ENV === 'development' ? 'dev' : 'combined';
-  const skip = (_: Request, res: Response) =>
-    format === 'combined' && res.statusCode < 400;
+const format = process.env.NODE_ENV === 'development' ? 'dev' : 'combined';
+const skip = (_: any, res: Response) =>
+  format === 'combined' && res.statusCode < 400;
 
-  return morgan(format, { skip });
-}
+export default morgan(format, { skip });
