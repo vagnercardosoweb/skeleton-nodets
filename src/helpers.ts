8982daa84a31896e2f8dcc1280c7e76c7bf9ae25
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-03-27 10:03:36 -0300

    feat: adiciona função para formatar bytes
    Formata bytes para olhos humanos entender

--- src/helpers.ts
+++ src/helpers.ts
@@ -279,3 +279,15 @@ export function renderView(template: string, context: object): string {
 
   return nunjucks.render(`${template}.njk`, context || {});
 }
+
+export function bytesToSize(bytes: number, decimals: number = 2) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`;
+}
